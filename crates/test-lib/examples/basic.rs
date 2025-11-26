c0b671ce1844ae9109040758d4e727c8e1650818
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-11-26 17:21:44 -0300

    feat: basic example directory
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

---
+++ crates/test-lib/examples/basic.rs
@@ -0,0 +1,12 @@
+// mod test {
+//     #[test]
+//     fn test() {
+//         println!("Checking that math still works");
+//         assert!(1, 2);
+//         println!("All good");
+//     }
+// }
+
+fn main() {
+    println!("Hello");
+}
--- examples/test.rs
+++
@@ -1,9 +0,0 @@
-fn test() {
-    println!("Checking that math still works");
-    assert!(1, 1);
-    println!("All good");
-}
-
-fn main() {
-    test()
-}
