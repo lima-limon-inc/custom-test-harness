f38fbc1c4defa91dc2d0571c43cbc35e29f0969f
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-11-27 18:36:53 -0300

    refactor: rename function names
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- crates/usecase/tests/foo.rs
+++ crates/usecase/tests/foo.rs
@@ -1,11 +1,17 @@
 use miden_harness_macros::miden_test;
 
 #[miden_test]
-fn jamon() {
+fn ham() {
     assert_eq!(1, 2)
 }
 
 #[miden_test]
-fn queso() {
+fn cheese() {
     assert_eq!(1, 1)
+}
+
+#[test]
+fn lettuce() {
+    println!("This test is never run despite marked as #[test]. This is because we are using our own custom test harness.");
+    assert_eq!(1, 3)
 }
