c38c9d4f223c47a59f79048ec41271c11f69b62a
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-11-27 16:15:01 -0300

    WIP
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- crates/test-lib/src/lib.rs
+++ crates/test-lib/src/lib.rs
@@ -1,12 +1,42 @@
-fn hello() {}
-
 // Only intended to be used by the macro crate
 pub struct MidenTest {
     pub name: &'static str,
-    pub test_fn: fn() -> Result<(), libtest_mimic::Failed>,
+    // pub test_fn: fn() -> Result<(), libtest_mimic::Failed>,
+    pub test_fn: fn() -> (),
 }
 
 inventory::collect!(MidenTest);
+
+// Wrapper used to make normal rust function.
+fn runner(test: fn() -> ()) -> impl FnOnce() -> Result<(), libtest_mimic::Failed> + Send + 'static {
+    move || {
+        test();
+        Ok(())
+    }
+}
+
+impl From<MidenTest> for libtest_mimic::Trial {
+    fn from(value: MidenTest) -> Self {
+        libtest_mimic::Trial::test(value.name, runner(value.test_fn))
+    }
+}
+
+pub struct MidenTestArguments(libtest_mimic::Arguments);
+
+impl From<MidenTestArguments> for libtest_mimic::Arguments {
+    fn from(value: MidenTestArguments) -> Self {
+        value.0
+    }
+}
+
+pub fn run(args: MidenTestArguments, tests: Vec<MidenTest>) {
+    let args = args.into();
+    let tests = tests.into_iter().map(|t| t.into()).collect();
+
+    let conclusion = libtest_mimic::run(&args, tests);
+
+    conclusion.exit()
+}
 
 // fn main() {
 //     println!("Hello")
