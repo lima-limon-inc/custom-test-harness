7dddf4073f3edc1fcd86ab3c825c4984db41a969
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-11-27 16:43:16 -0300

    WIP
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- crates/test-macros/src/lib.rs
+++ crates/test-macros/src/lib.rs
@@ -1,24 +1,20 @@
-use miden_test_harness::MidenTest;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, parse_quote, Item, ItemFn};
+use syn::{parse_macro_input, ItemFn};
 
 static mut PROCESSED: bool = false;
 
 #[proc_macro_attribute]
 pub fn miden_test(
-    attr: proc_macro::TokenStream,
+    _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    println!("attr: \"{attr}\"");
-    println!("item: \"{item}\"");
-
     let input_fn = parse_macro_input!(item as ItemFn);
 
     let fn_name_str = input_fn.sig.ident.to_string();
     let fn_name = input_fn.sig.ident.clone();
 
-    // We use PROCESSED in order to recreate C's #ifndef
+    // We use PROCESSED in order to recreate C's #ifndef, and thus only generate a single main function.
     let prelude = if unsafe { PROCESSED } {
         quote! {}
     } else {
@@ -38,7 +34,6 @@ pub fn miden_test(
         }
     };
 
-    // The prelude is only added once.
     let function = quote! {
         #prelude
 
--- crates/usecase/tests/foo.rs
+++ crates/usecase/tests/foo.rs
@@ -2,5 +2,10 @@ use midenc_harness_macros::miden_test;
 
 #[miden_test]
 fn jamon() {
-    ()
+    assert_eq!(1, 2)
+}
+
+#[miden_test]
+fn queso() {
+    assert_eq!(1, 1)
 }
