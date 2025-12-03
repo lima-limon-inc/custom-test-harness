090efd77d57c95cc4fd566006ec0924281f3abda
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-12-03 18:24:43 -0300

    feat: the loaded Package respects the passed in binding
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- crates/test-macros/src/lib.rs
+++ crates/test-macros/src/lib.rs
@@ -4,15 +4,70 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemFn};
 
+#[cfg(feature = "test-flag")]
+fn is_test() -> bool {
+    true
+}
+
+#[cfg(not(feature = "test-flag"))]
+fn is_test() -> bool {
+    // true // Used when debugging.
+    false
+}
+
+fn load_account(function: &mut syn::ItemFn) {
+    let mut found_packages_vars = Vec::new();
+
+    for arg in function.sig.inputs.iter() {
+        let syn::FnArg::Typed(arg) = arg else {
+            continue;
+        };
+        let syn::Type::Path(syn::TypePath { path, .. }) = *arg.ty.clone() else {
+            continue;
+        };
+        // The last token in the segments vector is the actual type, the rest
+        // are just path specifiers.
+        let Some(maybe_package) = path.segments.last() else {
+            continue;
+        };
+
+        if maybe_package.ident != "Package" {
+            continue;
+        }
+
+        let syn::Pat::Ident(package_var_binding) = arg.pat.as_ref() else {
+            panic!("Couldn't find binding for package")
+        };
+        found_packages_vars.push(package_var_binding.ident.clone());
+    }
+
+    let var_name = found_packages_vars[0].clone();
+
+    let load_package: Vec<syn::Stmt> = syn::parse_quote! {
+        let path = "/Users/fabri/Repositories/counter-contract_original/counter-contract/target/miden/debug/counter_contract.masp";
+        let bytes = std::fs::read(path).unwrap();
+        let #var_name = miden_mast_package::Package::read_from_bytes(&bytes).unwrap();
+    };
+
+    // We add the the lines required to load the generated Package.
+    for (i, package) in load_package.iter().enumerate() {
+        function.block.as_mut().stmts.insert(i, package.clone());
+    }
+}
+
 #[proc_macro_attribute]
 pub fn miden_test(
     _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let input_fn = parse_macro_input!(item as ItemFn);
+    let mut input_fn = parse_macro_input!(item as ItemFn);
 
     let fn_ident = input_fn.sig.ident.clone();
     let fn_name = fn_ident.clone().span().source_text().unwrap();
+
+    load_account(&mut input_fn);
+
+    input_fn.sig.inputs.clear();
 
     let function = quote! {
         miden_harness_lib::miden_test_submit!(
@@ -47,17 +102,6 @@ pub fn miden_test_block(
         .unwrap()
         .1
         .insert(0, internal_use);
-
-    #[cfg(feature = "test-flag")]
-    fn is_test() -> bool {
-        true
-    }
-
-    #[cfg(not(feature = "test-flag"))]
-    fn is_test() -> bool {
-        // true // Used when debugging.
-        false
-    }
 
     let module = if is_test() {
         quote! {
